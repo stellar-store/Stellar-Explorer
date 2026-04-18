be749338735051b6930fc37207ff8718d9d5fe0b
Stellar <gus.bonachea@outlook.com>
2026-04-17 22:53:14 -0300

    Add files via upload

---
+++ build/CMakeFiles/StellarExplorer_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StellarExplorer_autogen"
+  "StellarExplorer_autogen/mocs_compilation.cpp"
+  "StellarExplorer_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/StellarExplorer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
